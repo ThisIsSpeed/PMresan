5b09cd0309f2b553f82475de18244e42a970d085
ThisIsSpeed <thisisspeed5@gmail.com>
2016-07-12 15:50:22 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '194955654:AAFZo1yDeDoe91-0_y56iPhSU8qwjrmq7BE');
+define('BOT_TOKEN', '224992971:AAHHDabYxsHvUvj6jB87fKyA6XEpyCyaiP4');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +115,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = 238281750;
+  $admin = 202063007;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
